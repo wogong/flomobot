9db8f956704facd1b15892991ee9aabb8e03ef9b
Cheng Zhen <wogong38@gmail.com>
2020-12-14 01:06:42 +0800

    Update config-example.py

--- config-example.py
+++ config-example.py
@@ -1,3 +1,4 @@
-# substitude with your API below, you can find it in https://flomoapp.com/mine?source=incoming_webhook
+# Substitute with your API below, you can find it in https://flomoapp.com/mine?source=incoming_webhook
 FLOMO_API="https://flomoapp.com/iwh/xxxxxx/"
+# Substitute with your own Telegram Bot token
 BOT="1496058806:xxxxx"
